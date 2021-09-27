890d78872fe475c6450bf35aa008953406042486
Ramon Maximiliano Wilbert <55163563+RamonMaximiliano@users.noreply.github.com>
2021-09-27 18:10:44 -0300

    Errorupdates

--- maratona-discover-02-1.0.0/src/routes.js
+++ maratona-discover-02-1.0.0/src/routes.js
@@ -18,96 +18,117 @@ routes.get("/job/edit", (req, res) => res.sendFile(basePath + "/job-edit.html"))
 routes.get("/profile", (req, res) => res.sendFile(basePath + "/profile.html"))*/
 
 //criou um objeto, que sera enviado pro motor do ejs e então injetado no HTML
-const profile = {
-    name: "Ramon",
-    avatar: "https://avatars.githubusercontent.com/u/55163563?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75
+const Profile = {
+    data: {
+        name: "Ramon",
+        avatar: "https://avatars.githubusercontent.com/u/55163563?v=4",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 75
+    },
+    controllers: {
+       index(req, res){
+        return res.render(views + "profile", {profile: Profile.data})
+       },
+
+       update(req, res) {
+
+       }
+    },
+
 }
 
-//variavel criada para captar o objeto job { name: 'Test', 'daily-hours': '10', 'total-hours': '100' }
-const jobs = [
-    {
-        id: 1,
-        name: "Pizzaria Guloso",
-        "daily-hours": 2,
-        "total-hours": 60,
-        created_at: Date.now() // atribuindo data de hoje
+
+
+const Job = {
+    //variavel criada para captar o objeto job { name: 'Test', 'daily-hours': '10', 'total-hours': '100' }
+    data: [
+        [
+            {
+                id: 1,
+                name: "Pizzaria Guloso",
+                "daily-hours": 2,
+                "total-hours": 1,
+                created_at: Date.now() // atribuindo data de hoje
+            },
+            {
+                id: 2,
+                name: "OneTwo Project",
+                "daily-hours": 3,
+                "total-hours": 47,
+                created_at: Date.now() // atribuindo data de hoje
+            }
+        ] 
+    ],
+    controllers: {
+        index(req, res) {
+               const updatedJobs = Job.data.map((job) => {
+            
+                    const remaining = Job.services.remainingDays(job)
+                    const status = remaining <= 0 ? 'done' : 'progress'
+                    return {
+                        ...job,
+                        remaining,
+                        status,
+                        budget: Profile.data["value-hour"] * job["total-hours"]
+                    }
+                })
+            
+                return res.render(views + "index", { jobs: updatedJobs })
+        },
+
+        create(req, res){
+        return res.render(views + "job")
+        },
+        save(req, res){
+            //variavel criada para captar o objeto job { name: 'Test', 'daily-hours': '10', 'total-hours': '100' }
+            //ela busca no array jobs acima, o item de last id, caso não tenha nenhum dai coloca o 1 
+            //const lastId = jobs[jobs.length - 1]?.id || 1; com o ponto de interrogação ali não esta funcionando,verificar o porque
+            const lastId = Job.data[Job.data.length - 1].id || 1;
+
+            Job.data.push({
+                id: lastId + 1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                created_at: Date.now() // atribuindo data de hoje
+            })
+
+            //após adicionar o job ele redirecionará para pagina principal, por conta do '/'
+            return res.redirect('/')
+
+
+        }
     },
-    {
-        id: 2,
-        name: "OneTwo Project",
-        "daily-hours": 3,
-        "total-hours": 47,
-        created_at: Date.now() // atribuindo data de hoje
+    services: {
+         remainingDays(job){
+            //calculo de tempo restante para finalização do job
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+        
+            const createDate = new Date(job.created_at)
+            const dueDay = createDate.getDate() + Number(remainingDays)
+            const dueDateInMs = createDate.setDate(dueDay)
+        
+            const timeDiffInMs = dueDateInMs - Date.now()
+            // transformar milli em dias
+            const dayInMs = 1000 * 60 * 60 *24
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+            
+            //restam x dias 
+            return dayDiff
+        }
     }
-] 
-
-
-function remainingDays(job){
-    //calculo de tempo restante para finalização do job
-    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
-
-    const createDate = new Date(job.created_at)
-    const dueDay = createDate.getDate() + Number(remainingDays)
-    const dueDateInMs = createDate.setDate(dueDay)
-
-    const timeDiffInMs = dueDateInMs - Date.now()
-    // transformar milli em dias
-    const dayInMs = 1000 * 60 * 60 *24
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
-    
-    //restam x dias 
-    return dayDiff
 }
 
 //rotas abaixo após renderizar adicionado o ejs
-routes.get("/", (req, res) => {
-    //ajustes no job
-    // calculo tempo restante
-
-    const updatedJobs = jobs.map((job) => {
-
-        const remaining = remainingDays(job)
-        const status = remaining <= 0 ? 'done' : 'progress'
-        return {
-            ...job,
-            remaining,
-            status,
-            budget: profile["value-hour"] * job["total-hours"]
-        }
-    })
-
-    return res.render(views + "index", { jobs: updatedJobs })
-})
-
-
-
-
-routes.get("/job", (req, res) => res.render(views + "job"))
-routes.post("/job", (req, res) => {
-    //variavel criada para captar o objeto job { name: 'Test', 'daily-hours': '10', 'total-hours': '100' }
-    //ela busca no array jobs acima, o item de last id, caso não tenha nenhum dai coloca o 1 
-    //const lastId = jobs[jobs.length - 1]?.id || 1; com o ponto de interrogação ali não esta funcionando,verificar o porque
-   const lastId = jobs[jobs.length - 1].id || 1;
-
-    jobs.push({
-        id: lastId + 1,
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        created_at: Date.now() // atribuindo data de hoje
-    })
-
-    //após adicionar o job ele redirecionará para pagina principal, por conta do '/'
-    return res.redirect('/')
-})
+routes.get("/", Job.controllers.index)
+routes.get("/job", Job.controllers.create)
+routes.post("/job", Job.controllers.save)
 
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"))
-routes.get("/profile", (req, res) => res.render(views + "profile", {profile: profile}))
+routes.get("/profile", Profile.controllers.update)
 
 module.exports = routes;
 
