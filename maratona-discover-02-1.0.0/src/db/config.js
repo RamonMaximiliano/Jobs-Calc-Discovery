177fd02081c5593254ca975d6c23e0a694f34faf
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2021-10-11 22:48:23 -0300

    DbCreation

--- maratona-discover-02-1.0.0/src/db/config.js
+++ maratona-discover-02-1.0.0/src/db/config.js
@@ -3,12 +3,7 @@ const sqlite3 = require('sqlite3')
 const { open } = require('sqlite')
 
 
-module.exports = () => {
-open({
+module.exports = () => open({
     filename: './database.sqlite',
-    driver: sqlite3.Database
+    driver: sqlite3.Database,
 });
-};
-
-
-
---
+++ maratona-discover-02-1.0.0/src/db/init.js
@@ -0,0 +1,76 @@
+const Database = require('./config')
+
+
+//o await do JS precisa estar dentro de um async, o await faz ele esperar cada comando acabar para então executar o proximo
+const initDb = {
+    async init(){
+
+const db = await Database()
+
+await db.exec(`CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    avatar TEXT,
+    monthly_budget INT,
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
+)`)
+
+await db.exec(`CREATE TABLE jobs (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT,
+daily_hours INT,
+total_hours INT,
+created_at DATETIME
+)`)
+
+await db.run(`
+    INSERT INTO profile (
+        name,
+        avatar,
+        monthly_budget,
+        days_per_week,
+        hours_per_day,
+        vacation_per_year
+        ) VALUES (
+            "Ramon",
+            "https://avatars.githubusercontent.com/u/55163563?v=4",
+            3000,
+            5,
+            5,
+            4
+         )`)
+
+await db.run(`
+INSERT INTO jobs (
+    name,
+    daily_hours,
+    total_hours,
+    created_at
+    ) VALUES (
+        "Pizzaria GUloso",
+        2,
+        1,
+        1617514376018
+    )`)
+
+await db.run(`
+   INSERT INTO jobs (
+        name,
+        daily_hours,
+        total_hours,
+        created_at
+        ) VALUES (
+            "OneTwo Projects",
+            3,
+            47,
+            1617514376018
+        )`)
+
+await db.close()
+}
+}
+
+initDb.init
